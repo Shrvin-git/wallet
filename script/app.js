eb5ce8743380b58653da23d1ad59285b48de7584
Shrvin-git <shrvindavoodi@gmail.com>
2024-06-19 17:02:31 +0330

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {});
+
+
+
+const navToggleIcon = document.querySelector('.nav__btn-toggle')
+const menu = document.querySelector('.nav-menu')
+
+navToggleIcon.addEventListener('click', function () {
+    this.classList.toggle('nav__toggle-icon--open');
+    menu.classList.toggle('menu--open');
+
+})
